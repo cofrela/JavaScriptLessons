ad70b57c68668505f8922dbf0569aed9f2308724
cofrela <cofrela@gmail.com>
2017-05-14 17:47:21 +0700

    Calculating the area of a rectangle

---
+++ areaRectangle.js
@@ -0,0 +1,10 @@
+function showMessage() {
+    alert("Вы щелкнули по диву");
+}
+
+function areaRectangle(obj) {
+    var a = obj.t1.value;
+    var b = obj.t2.value;
+    var s = a * b;
+    obj.res.value = s;
+}
