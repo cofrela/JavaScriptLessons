ac65ce91b6bd2a474affe586c5a877d659ac622e
cofrela <cofrela@gmail.com>
2017-06-04 13:46:36 +0700

    method confirm()

---
+++ Открытие окна confirm/script.js
@@ -0,0 +1,7 @@
+
+function choiceOf() {
+    if (confirm("Вы действительно хотите открыть окно?")) {
+      volvo=window.open("volvo.html", "display_volvo",
+            "width=400,height=300,status=no,toolbar=no,menubar=no");
+   }
+}
