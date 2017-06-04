05b8b67c0f69f8f84e8b7625570ee9cc9dbffb43
cofrela <cofrela@gmail.com>
2017-06-04 13:36:44 +0700

    методы open() и close() window

---
+++ Открытие и закрытие окон/script.js
@@ -0,0 +1,16 @@
+
+function open_ford() {
+    ford = window.open("ford.html", "display_ford", "width=400,height=300,status=no,toolbar=no,menubar=no");
+}
+
+function open_mazda() {
+    mazda = window.open("mazda.html", "display_mazda", "width=400,height=300,status=no,toolbar=no,menubar=no");
+}
+
+function open_volvo() {
+    volvo = window.open("volvo.html", "display_volvo", "width=400,height=300,status=no,toolbar=no,menubar=no");
+}
+
+function close_pict() {
+    window.close();
+}
